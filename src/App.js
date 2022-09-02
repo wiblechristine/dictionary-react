b40c6dd06a83b46b87a1d1daeda9976974b848d4
Christine <midnighter07@gmail.com>
2022-09-02 12:26:41 -0400

    search engine set up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 
 import './App.css';
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-      Hello
-        
+      Hello!
       </header>
+      <main>
+        <Dictionary/>
+      </main>
+      <footer className="App-footer">
+        <small>
+        Coded by Christine Wible
+        </small>
+      </footer>
+    </div>
     </div>
   );
 }
 
-export default App;
+
